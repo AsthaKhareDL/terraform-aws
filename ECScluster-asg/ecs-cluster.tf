9915fbbe3d9df161536e9e5a6959d67fe8ff59ea
AsthakhareDL <asthamay28@gmail.com>
2026-08-22 03:51:55 +0530

    added ssm policy

--- ECScluster-asg/ecs-cluster.tf
+++ ECScluster-asg/ecs-cluster.tf
@@ -30,7 +30,10 @@ resource "aws_iam_role" "ecs_instance_role" {
     ]
   })
 }
-
+resource "aws_iam_role_policy_attachment" "ecs_ssm" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
 
 # ============================================================
 # ECS EC2 IAM Policy
